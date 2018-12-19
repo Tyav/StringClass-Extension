f64dc041e90f8a4a8691caffe0ccddf3c4354946
Tyav <tyav2greenz@gmail.com>
2018-12-19 15:46:32 +0100

    added test for toUpper method

--- StringClassExtensionSolution.test.js
+++ StringClassExtensionSolution.test.js
@@ -1,6 +1,6 @@
 require('./StringClassExtensionSolution');
 
-describe('Test for Vowels', () => {
+describe('Test for hasVowels', () => {
     test('A simple case where no vowel exist', () => {
         expect('whtdsn'.hasVowels()).toBeFalsy();
     });
@@ -8,4 +8,17 @@ describe('Test for Vowels', () => {
         expect('Amidso'.hasVowels()).toBeTruthy();
     });
 });
-
+describe('Test for toUpper', () => {
+    test('A simple case where all the characters are lower', () => {
+        expect('james'.toUpper()).toBe('JAMES');
+    });
+    test('A case where the first character is upper', () => {
+        expect('Amidso'.toUpper()).toBe('AMIDSO');
+    });
+    test('A case where some of the characters are lower', () => {
+        expect('MowSer'.toUpper()).toBe('MOWSER');
+    });
+    test('A case where there are none alpha characters in the string', () => {
+        expect('53j-t4ie3s'.toUpper()).toMatch(/^53J-T4IE3S$/)
+    })
+});
