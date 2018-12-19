545346cb65140cbe3f094b9dff53bfeb2a9c1d8d
Tyav <tyav2greenz@gmail.com>
2018-12-19 16:38:32 +0100

    solved the toLower test case

--- StringClassExtensionSolution.js
+++ StringClassExtensionSolution.js
@@ -13,7 +13,15 @@ String.prototype.toUpper = function () {
     }
     return string.join('')
 }
-
+String.prototype.toLower = function () {
+    let string = Object.values(this);
+    for (var i = 0; i < string.length; i++){
+        if (/[A-Z]+$/u.test(string[i])){
+            string[i] = String.fromCharCode(string[i].charCodeAt(0) + 32)
+        }
+    }
+    return string.join('')
+}
 
 
 
